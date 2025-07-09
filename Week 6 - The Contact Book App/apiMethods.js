ab31d53e8d7c57db701f1e397791fdfb4d85ff71
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-09 17:40:31 +0200

    Lesson 18

--- Week 6 - The Contact Book App/apiMethods.js
+++ Week 6 - The Contact Book App/apiMethods.js
@@ -63,9 +63,39 @@ function editContactLink(id) {
     window.open("edit-contact.html?id=" + id, "_self");
 }
 
+var id = getId();
+console.log(id);
+
 function getId() {
     var url = window.location.href;
     var position = url.search("=");
     var id = url.slice(position + 1);
     return id;
+}
+
+function getContact(){
+    fetch(rootPath + "controller/get-contacts/?id=" + id).then(function(response) {
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        //displayOutput(data);
+    })
+
+}
+
+function displayContactOutput(data) {
+    output = "<table>";
+
+    for(item in data){
+        output += `
+            <tr onclick="editContactLink(${data[item].id})">
+                <td><img src="${rootPath}controller/uploads/${data[item].avatar}" width="40"/></td>
+                <td><h5>${data[item].firstname}</h5></td>
+                <td><h5>${data[item].lastname}</h5></td>
+            </tr>
+        `
+    }
+
+    output += "</table>";
+    document.getElementById("table").innerHTML = output;
 }
