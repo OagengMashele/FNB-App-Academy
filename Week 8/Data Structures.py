f3d18ffe703542983dd8691d9898c32c26a35b26
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 17:57:15 +0200

    lesson 3: tuples

--- Week 8/Data Structures.py
+++ Week 8/Data Structures.py
@@ -27,3 +27,25 @@ fruits.sort()
 print(fruits)
 fruits.sort(reverse=True)
 print(fruits)
+
+#Tuples
+# cannot change,no duplicates
+
+my_tuple = (1,5,2,3,4,6)
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[4])
+print(my_tuple[-1])
+
+#concatination and repetition
+tuple1 = (1,2,3)
+tuple2 =(4,5,6)
+
+print(tuple1)
+print(tuple2)
+
+conc_tuple = tuple1 + tuple2
+print(conc_tuple)
+
+rep_tuple = tuple1 * 2
+print(rep_tuple)
