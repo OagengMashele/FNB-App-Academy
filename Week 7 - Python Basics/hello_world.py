a84387f843a1fccd0da8e779f02371643f85ec05
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 11:10:46 +0200

    Lesson 4

--- Week 7 - Python Basics/hello_world.py
+++ Week 7 - Python Basics/hello_world.py
@@ -13,3 +13,12 @@ print(multilineString)
 print(message[0])
 print(message[-1])
 print(len(message))
+
+
+#Numerics
+num = 5
+numtype = type(num)
+num1= 5.5
+num1Type = type(num1)
+print(num1)
+print(num1Type)
