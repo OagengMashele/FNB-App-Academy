0bbd80c88fd6771e33d05ff9f96683c83bb4d63e
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 11:02:26 +0200

    lesson  strings

--- Week 7 - Python Basics/hello_world.py
+++ Week 7 - Python Basics/hello_world.py
@@ -1,1 +1,11 @@
 print("Hello World!!")
+
+#Strings
+message = 'Hello world'
+anotherMessage = 'this is a string'
+doubleMessage = "This is also a string"
+anotherString = "tests's tesing string"
+multilineString = """This is a
+multiLineString"""
+print(message)
+print(multilineString)
