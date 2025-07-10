09f84ff164f52050f8ac3a3a9653aa1122a44442
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 19:06:09 +0200

    Lesson 3: importing modules

--- Week 8 - 9/modules.py
+++ Week 8 - 9/modules.py
@@ -1,5 +1,8 @@
 #Modules
 import math_operations
+import math
+import random
+
 
 print(math_operations.add(1,1))
 
@@ -8,3 +11,9 @@ print(math_operations.minus(1,1))
 print(math_operations.times(1,1))
 
 print(math_operations.divide(1,1))
+
+print(math.pi)
+
+print(random.randint(0,7))
+
+
