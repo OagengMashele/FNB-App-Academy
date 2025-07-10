b9aa5e23f349f1c97d9b184c2d66f5967b912d2f
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 17:50:09 +0200

    Lesson 2

--- Week 8/Data Structures.py
+++ Week 8/Data Structures.py
@@ -9,3 +9,21 @@ fruits[1] = "blackberry"
 
 print(fruits)
 print(fruits[1])
+
+#adding items to list
+
+print(fruits)
+fruits.append("strawberry")
+print(fruits)
+
+fruits.insert(1, "grape")
+print(fruits)
+
+#remove from list
+fruits.remove('apple')
+print(fruits)
+
+fruits.sort()
+print(fruits)
+fruits.sort(reverse=True)
+print(fruits)
