ceb14df2f621e22c25f6d0be13e1b30dc493f4fe
Oageng Mashele <oagengmashele86@gmail.com>
2025-07-10 18:04:46 +0200

    Lesson 4: Sets

--- Week 8/Data Structures.py
+++ Week 8/Data Structures.py
@@ -29,7 +29,7 @@ fruits.sort(reverse=True)
 print(fruits)
 
 #Tuples
-# cannot change,no duplicates
+# cannot change
 
 my_tuple = (1,5,2,3,4,6)
 print(my_tuple)
@@ -49,3 +49,31 @@ print(conc_tuple)
 
 rep_tuple = tuple1 * 2
 print(rep_tuple)
+
+#Sets
+#no duplicates
+
+my_set = {1,2,3,4,5,"apple"}
+print(my_set)
+
+my_set.add("banana")
+print(my_set)
+my_set.remove(2)
+print(my_set)
+
+set1 = {1,2,3}
+set2 = {3,4,5}
+
+print(set1)
+print(set2)
+#Union
+union_set = set1.union(set2)
+print(union_set)
+#intersection
+intersect_set = set1.intersection(set2)
+print(intersect_set)
+#difference
+diff_set = set1.difference(set2)
+difff_set = set2.difference(set1)
+print(diff_set)
+print(difff_set)
